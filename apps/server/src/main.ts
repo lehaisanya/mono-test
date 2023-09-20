3711efb4926fc9ef666016ab4e9e3de32d278b2d
lehaisanya <lehaisanya@gmail.com>
2023-09-20 20:06:10 +0300

    Add static

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,13 +3,14 @@ import cors from 'cors';
 import { createExpressMiddleware } from '@trpc/server/adapters/express';
 import { createContext, appRouter } from '@mono-test/routes';
 import { connectToDb } from '@mono-test/db';
-import { createHTTPServer } from '@trpc/server/adapters/standalone';
 
 const PORT = process.env.PORT ?? 3000;
 
 const app = express();
 
 app.use(express.json());
+
+app.use(express.static('../../../dist/apps/client'));
 
 if (process.env.NODE_ENV === 'development') {
   app.use(cors());
@@ -24,20 +25,9 @@ app.use(
   '/api',
   createExpressMiddleware({
     router: appRouter,
-    createContext,
-    middleware: cors()
+    createContext
   })
 );
-
-app.use('/hel', (req, res) => {
-  res.send('Hello');
-});
-
-// createHTTPServer({
-//   router: appRouter,
-//   createContext,
-//   middleware: cors()
-// }).listen(3000);
 
 const server = app.listen(PORT, () => {
   console.log(`Server start on http://localhost:${PORT}`);
